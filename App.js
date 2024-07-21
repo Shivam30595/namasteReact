b67079d147f6046984e43d445d26d84bdccdd39f
Shivam <shivamgupta30595@gmail.com>
2024-07-21 13:13:15 +0530

    Ignited our App

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement(
     "h1",
     { id: "heading" }, //attributes
-     "Hello World from React!"
+     "Hello World from React! By Shivam!!"
 ); // creating heading by using react
 console.log(heading) // object
 const root = ReactDOM.createRoot(document.getElementById("root"));// creating root using React DOM...We used React DOM because root is in DOM
